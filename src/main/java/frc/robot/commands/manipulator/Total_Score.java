e9042007956c74202ed3c6492725b9a461bfb39c
Advika <chanvis0709@gmail.com>
2025-04-02 16:57:42 -0500

    Maybe we can shoot level during auton?

--- src/main/java/frc/robot/commands/manipulator/Total_Score.java
+++ src/main/java/frc/robot/commands/manipulator/Total_Score.java
@@ -20,8 +20,8 @@ public class Total_Score extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
 
-    ElevatorSetPos elevatorsetpos = new ElevatorSetPos(elevatorgoalCounts);
-    // CoralForTime coralfortime = new CoralForTime(-5, 1.0);
+    // ElevatorSetPos elevatorsetpos = new ElevatorSetPos(elevatorgoalCounts);
+    CoralForTime coralfortime = new CoralForTime(-5, 1.0);
     // Wrist_SetPos wristsetpos = new Wrist_SetPos(wristgoalCounts);
     // ElevatorSetPos intakeElevatorSetPos = new
     // ElevatorSetPos(ElevatorConstants.intakeEncoderCounts);
@@ -30,6 +30,6 @@ public class Total_Score extends SequentialCommandGroup {
 
     // ParallelCommandGroup both = new ParallelCommandGroup(coral, wristsetpos);
 
-    addCommands(elevatorsetpos);
+    addCommands(coralfortime);
   }
 }
